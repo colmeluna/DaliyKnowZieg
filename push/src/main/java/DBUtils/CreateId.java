a87a780033af604cd299bbc6645906e164509552
colmeluna <ziegler33@126.com>
2016-07-14 10:29:07 +0800

    计算执行时间 ms、ns

--- push/src/main/java/DBUtils/CreateId.java
+++ push/src/main/java/DBUtils/CreateId.java
@@ -4,9 +4,30 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Properties;
+import java.sql.SQLException;
 
 
+
+
+/**
+ * 
+ * //
+ * ①
+ * 毫秒计算时间间隔 
+ * 
+	long startTime=System.currentTimeMillis();   //获取开始时间  
+	doSomeThing();  //测试的代码段  
+	long endTime=System.currentTimeMillis(); //获取结束时间  
+	System.out.println("程序运行时间： "+(end-start)+"ms");
+         ②
+          纳秒计算时间间隔
+    long startTime=System.nanoTime();   //获取开始时间  
+	doSomeThing();  //测试的代码段  
+	long endTime=System.nanoTime(); //获取结束时间  
+	System.out.println("程序运行时间： "+(end-start)+"ns");   	
+ * @author Administrator
+ *
+ */
 public class CreateId {
 
 	public static Long newsId()
@@ -72,14 +93,26 @@ public class CreateId {
 	        		//Global.getConfig("jdbc.username");// 用户名,系统默认的账户名
 	        String password = "mao963(rtsFP2";
 	        		//Global.getConfig("jdbc.password");// 你安装时选设置的密码
-	        con = DriverManager.getConnection(url, user, password);// 获取连接
-	        String sql = "SELECT username from m_user where id = '1493' ";// 预编译语句，“？”代表参数
-	        pre = con.prepareStatement(sql);// 实例化预编译语句
-	        result = pre.executeQuery();// 执行查询，注意括号中不需要再加参数
-	        if (result.next()){
-	        	return  result.getString("username");
+	        long startTime=System.nanoTime();   //获取开始时间  
+	        long startTimeMs=System.currentTimeMillis();   //获取开始时间  
+		        con = DriverManager.getConnection(url, user, password);// 获取连接
+		        System.out.println("建立数据库连接成功 ");
+		        //String sql = "SELECT username from m_user where id = '1493' ";// 预编译语句，“？”代表参数
+		        String sql = "select * from s_product";
+		        pre = con.prepareStatement(sql);// 实例化预编译语句
+		        result = pre.executeQuery();// 执行查询，注意括号中不需要再加参数
+		        System.out.println("执行查询完毕");
+	        long endTime=System.nanoTime(); //获取结束时间  
+	        long endTimeMs=System.currentTimeMillis(); //获取结束时间  
+	    		System.out.println("程序运行时间： "+(endTimeMs-startTimeMs)+"ms");
+	        	System.out.println("程序运行时间： "+(endTime-startTime)+"ns"); 
+	        	if (result.next()){
+	        	return  result.getString("id");
 	        			//(long) result.getInt("id");
 	        	
+	        } 
+	        while(result.next()){
+	        	System.out.println(result.getString("id"));
 	        }
 	    }
 	    catch (Exception e)
@@ -108,10 +141,15 @@ public class CreateId {
 		return null;
 	}
 	
+	public Connection Connet(String url,String user,String password) throws SQLException{
+		Connection  con = DriverManager.getConnection(url, user, password);// 获取连接
+		return con;
+	}
+	
 	public static void main(String args[]){
-		Long id  = CreateId.newsId();
+//		Long id  = CreateId.newsId();
 		 String username = CreateId.getStrforFreeSql();
-		System.out.println(id);
+//		System.out.println(id);
 		 System.out.println(username);
 	}
 }
